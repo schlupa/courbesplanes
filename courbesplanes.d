354b968231a74d4ec4ffb59cbd70e21db7cd736b
Patrick Schluter <Patrick.Schluter@bbox.fr>
2018-12-01 20:41:21 +0100

    Translation by rote

--- courbesplanes.d
+++ courbesplanes.d
@@ -17,40 +17,7 @@
 300 XX = X * P4 + Y * P5 - Z * P3
 310 YY = Y * P1 - X * P2
 320  RETURN 
-330  HCOLOR= 3: TEXT : CLEAR :YP = 0:XP = 0:XX = 0:YY = 0:XD = 0:YD = 0:S = 0:NX = 0:NY = 0:Y = 0:X = 0:PI = 3.1415925
-340 XB = 0:YB = 0:YS = 0:XA = 0:YA = 0:XS = 0:Y0 = 0:NL = 0:Z = 0:Z0 = 0:Z1 = 0
-350 HH = 191
-360 BR = 279
-370  DIM O%(BR - 1),U%(BR - 1)
-380  HOME : PRINT  TAB( 12);"FONCTIONS DE L'ESPACE"
-390  PRINT : PRINT : PRINT "LE PROGRAMME NECESSITE UNE FONCTION"
-400  PRINT "DE LA FORME";: INVERSE : PRINT " 200 Z=F(X,Y) ";: NORMAL : PRINT "."
-410  PRINT "UNE REPRESENTATION TRIDIMENSIONNELLE"
-420  PRINT "EN SERA ETABLIE A L'ECRAN."
-430  PRINT 
-440  PRINT : PRINT "LA FONCTION EST-ELLE DEJA DEFINIE ";: GOSUB 2000
-450  IF A$ = "O" THEN 470
-460  END 
-470  FOR I = 0 TO BR - 1:O%(I) =  - 1: NEXT 
-480  PRINT : PRINT 
-490  INPUT "NOMBRE DE MERIDIENS:";NL
-500  DIM PX%(NL,NL),PY%(NL,NL),X((NL + 1) ^ 2 - 1),Y((NL + 1) ^ 2 - 1)
-510  PRINT : INPUT "DEBUT D'INTERVALLE SUR X:";X0
-520  INPUT "FIN D'INTERVALLE SUR X:";X1
-530  PRINT 
-540  IF X1 <  = X0 THEN 510
-550  INPUT "DEBUT D'INTERVALLE SUR Y:";Y0
-560  INPUT "FIN D'INTERVALLE SUR Y:";Y1
-570  PRINT 
-580  IF Y1 <  = Y0 THEN 550
-590  PRINT "ANGLE DE VISION DU COTE"
-600  INPUT "(AZIMUTE 0-80)";AZ
-610  PRINT 
-620  IF AZ < 0 OR AZ > 80 THEN 590
-630  INPUT "INCLINAISON, HAUTEUR (0-90)";PH
-640  PRINT 
-650  IF PH < 0 OR PH > 90 THEN 630
-660  PRINT 
+
 670 P1 =  COS (AZ * PI / 180):P2 =  SIN (AZ * PI / 180)
 680 P3 =  COS (PH * PI / 180)
 690 P4 = P1 *  SIN (PH * PI / 180)
@@ -89,11 +56,85 @@
 1020  TEXT : PRINT "J'AI FINI LES CALCULS."
 1030  GET A$: IF A$ = " " THEN  END 
 1040  GOTO 1030
-2000  REM 
-2010  GET A$: IF A$ <  > "O" AND A$ <  > "N" THEN 2010
-2020  IF A$ = "O" THEN  PRINT "OUI": RETURN 
-2030  PRINT "NON": RETURN 
 */
+import stdio;
+
+string A;
+
+float YP = 0;
+float XP = 0;
+float XX = 0;
+float YY = 0;
+float XD = 0;
+float YD = 0;
+float S = 0;
+float NX = 0;
+float NY = 0;
+float Y = 0;
+float X = 0;
+float PI = 3.1415925;
+float XB = 0;                                       // 340 XB = 0:YB = 0:YS = 0:XA = 0:YA = 0:XS = 0:Y0 = 0:NL = 0:Z = 0:Z0 = 0:Z1 = 0
+float YB = 0;
+float YS = 0;
+float XA = 0;
+float YA = 0;
+float XS = 0;
+float Y0 = 0;
+float NL = 0;
+float Z = 0;
+float Z0 = 0;
+float Z1 = 0;
+uint HH = 191;                                      // 350 HH = 191
+uint BR = 279;                                      // 360 BR = 279
+int[BR-1] O;                                        // 370  DIM O%(BR - 1),U%(BR - 1)
+int[BR-1] U;
+int[][] PX, PY;                                     // 500  DIM PX%(NL,NL),PY%(NL,NL)
+float[] X, Y;                                       // 500  DIM X((NL + 1) ^ 2 - 1),Y((NL + 1) ^ 2 - 1)
+
+void GOTO470()
+{
+    foreach(i; O) O[i] = -1;                        // 470  FOR I = 0 TO BR - 1:O%(I) =  - 1: NEXT 
+    writeln("\n");                                  // 480  PRINT : PRINT 
+    INPUT("NOMBRE DE MERIDIENS:", NL);              // 490  INPUT "NOMBRE DE MERIDIENS:";NL
+    PX = new int[][](NL, NL);                       // 500  DIM PX%(NL,NL),PY%(NL,NL),X((NL + 1) ^ 2 - 1),Y((NL + 1) ^ 2 - 1)
+    PY = new int[][](NL, NL);
+    X = new float[](NL+1);
+    Y = new float[](NL+1);
+    do {
+        INPUT("\nDEBUT D'INTERVALLE SUR X:", X0);   // 510  PRINT : INPUT "DEBUT D'INTERVALLE SUR X:";X0
+        INPUT("FIN D'INTERVALLE SUR X:", X1);       // 520  INPUT "FIN D'INTERVALLE SUR X:";X1
+        writeln;                                    // 530  PRINT 
+    } while(X1 <= X0);                              // 540  IF X1 <  = X0 THEN 510
+    do {
+        INPUT("DEBUT D'INTERVALLE SUR Y:", Y0);     // 550  INPUT "DEBUT D'INTERVALLE SUR Y:";Y0
+        INPUT("FIN D'INTERVALLE SUR Y:", Y1);       // 560  INPUT "FIN D'INTERVALLE SUR Y:";Y1
+        writeln;                                    // 570  PRINT 
+    } while(Y1 <= Y0);                              // 580  IF Y1 <  = Y0 THEN 550
+    do {
+        writeln("ANGLE DE VISION DU COTE");         // 590  PRINT "ANGLE DE VISION DU COTE"
+        INPUT("(AZIMUTE 0-80)", AZ);                // 600  INPUT "(AZIMUTE 0-80)";AZ
+        writeln;                                    // 610  PRINT 
+    } while(AZ < 0 && AZ > 80);                     // 620  IF AZ < 0 OR AZ > 80 THEN 590
+    do {
+        INPUT("INCLINAISON, HAUTEUR (0-90)", PH);   // 630  INPUT "INCLINAISON, HAUTEUR (0-90)";PH
+        writeln;                                    // 640  PRINT 
+    } while(PH < 0 && PH > 90);                     // 650  IF PH < 0 OR PH > 90 THEN 630
+        writeln;                                    // 660  PRINT 
+
+}
+
+/* Read a response from the keyboard */
+void GOSUB2000()                    // 2000 REM
+{
+    do {                            // 2010  GET A$: IF A$ <  > "O" AND A$ <  > "N" THEN 2010
+        A = stdin.read();
+    } while(A != 'O' && A != 'N');
+    if(A == 'O') {                  // 2020  IF A$ = "O" THEN  PRINT "OUI": RETURN
+        writeln("OUI");
+        return;
+    }
+    writeln("NON");                 // 2030  PRINT "NON": RETURN
+}
 
 /******************************************************************************/
 /**
@@ -105,40 +146,18 @@
 /******************************************************************************/
 int main(string[] args)
 {
-    HCOLOR= 3;
+    HCOLOR= 3;                                          // 330  HCOLOR= 3: TEXT : CLEAR :YP = 0:XP = 0:XX = 0:YY = 0:XD = 0:YD = 0:S = 0:NX = 0:NY = 0:Y = 0:X = 0:PI = 3.1415925
     TEXT;
     CLEAR;
-    YP = 0;
-    XP = 0;
-    XX = 0;
-    YY = 0;
-    XD = 0;
-    YD = 0;
-    S = 0;
-    NX = 0;
-    NY = 0;
-    Y = 0;
-    X = 0;
-    PI = 3.1415925
-    XB = 0;
-    YB = 0;
-    YS = 0;
-    XA = 0;
-    YA = 0;
-    XS = 0;
-    Y0 = 0:NL = 0:Z = 0:Z0 = 0:Z1 = 0
-350 HH = 191
-360 BR = 279
-370  DIM O%(BR - 1),U%(BR - 1)
-380  HOME : PRINT  TAB( 12);"FONCTIONS DE L'ESPACE"
-390  PRINT : PRINT : PRINT "LE PROGRAMME NECESSITE UNE FONCTION"
-400  PRINT "DE LA FORME";: INVERSE : PRINT " 200 Z=F(X,Y) ";: NORMAL : PRINT "."
-410  PRINT "UNE REPRESENTATION TRIDIMENSIONNELLE"
-420  PRINT "EN SERA ETABLIE A L'ECRAN."
-430  PRINT 
-440  PRINT : PRINT "LA FONCTION EST-ELLE DEJA DEFINIE ";: GOSUB 2000
-450  IF A$ = "O" THEN 470
-460  END 
-
-  return 0;
+    HOME;
+    writeln("FONCTIONS DE L'ESPACE\n\n");               // 380  HOME : PRINT  TAB( 12);"FONCTIONS DE L'ESPACE"
+    writeln("LE PROGRAMME NECESSITE UNE FONCTION");     // 390  PRINT : PRINT : PRINT "LE PROGRAMME NECESSITE UNE FONCTION"
+    writeln("DE LA FORME Z=F(X,Y).");                   // 400  PRINT "DE LA FORME";: INVERSE : PRINT " 200 Z=F(X,Y) ";: NORMAL : PRINT "."
+    writeln("UNE REPRESENTATION TRIDIMENSIONNELLE");    // 410  PRINT "UNE REPRESENTATION TRIDIMENSIONNELLE"
+    writeln("EN SERA ETABLIE A L'ECRAN.\n\n");          // 420  PRINT "EN SERA ETABLIE A L'ECRAN."
+    writeln("LA FONCTION EST-ELLE DEJA DEFINIE ";       // 440  PRINT : PRINT "LA FONCTION EST-ELLE DEJA DEFINIE ";: GOSUB 2000
+    GOSUB2000();
+    if(A == 'O')                                        // 150  IF A$ = "O" THEN 470
+      THEN GOTO470;
+    return 0;                                           // 460  END 
 }
