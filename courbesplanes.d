17956d6793e352bf0ca5e34aa9b995994ef84f13
Patrick Schluter <Patrick.Schluter@ec.europa.eu>
2018-12-03 16:04:32 +0100

    GOTO670 minmaxZ

--- courbesplanes.d
+++ courbesplanes.d
@@ -4,7 +4,7 @@
 40  GOTO 60
 50  IF XA > XB THEN X = XA:XA = XB:XB = X:Y = YA:YA = YB:YB = Y
 60 XD = XB - XA:YD = YB - YA:S =  ABS (XD): IF  ABS (YD) > S THEN S =  ABS (YD)
-70  IF S = 0 THEN  RETURN 
+70  IF S = 0 THEN  RETURN
 80 XD = XD / S:YD = YD / S:XX = XA - XD:YY = YA - YD
 90  FOR I = 0 TO S:XX = XX + XD:YY = YY + YD:XP =  INT (XX + .5):YP =  INT (YY + .5)
 100  IF O%(YP) < 0 THEN O%(YP) = XP:U%(YP) = XP: GOTO 140
@@ -12,49 +12,31 @@
 120  IF XP < U%(YP) THEN U%(YP) = XP: GOTO 140
 130  GOTO 150
 140  HPLOT YP,XP
-150  NEXT : RETURN 
+150  NEXT : RETURN
 200 Z = 0:R = X * X + Y * Y: IF R < 4 THEN Z =  -  SQR (4 - R)
 300 XX = X * P4 + Y * P5 - Z * P3
 310 YY = Y * P1 - X * P2
-320  RETURN 
+320  RETURN
 
-670 P1 =  COS (AZ * PI / 180):P2 =  SIN (AZ * PI / 180)
-680 P3 =  COS (PH * PI / 180)
-690 P4 = P1 *  SIN (PH * PI / 180)
-700 P5 = P2 *  SIN (PH * PI / 180)
-710  PRINT "JE CHERCHE MAXIMUM ET MINIMUM."
-720  PRINT 
-730 XS = (X1 - X0) / NL:YS = (Y1 - Y0) / NL
-740 X = X0:Y = Y0: GOSUB 200:Z0 = Z:Z1 = Z:XA = XX:XB = XX:YA = YY:YB = YY:PT = 0
-750 X = X0 - XS: FOR NX = 0 TO NL:X = X + XS:Y = Y0 - YS: FOR NY = 0 TO NL:Y = Y + YS: GOSUB 200:X(PT) = XX:Y(PT) = YY: POKE 1024,NX: POKE 1025,NY
-760  IF Z > Z1 THEN Z1 = Z: GOTO 780
-770  IF Z < Z0 THEN Z0 = Z
-780  IF XA > XX THEN XA = XX: GOTO 800
-790  IF XX > XB THEN XB = XX
-800  IF YA > YY THEN YA = YY: GOTO 820
-810  IF YY > YB THEN YB = YY
-820 PT = PT + 1: NEXT : NEXT 
-830  PRINT "MINIMUM Z ";Z0
-840  PRINT "MAXIMUM Z ";Z1
-850  PRINT 
+850  PRINT
 860  PRINT "JE CALCULE UN CHAMP DE DONNEES"
 870  PRINT "NORMEES."
-880  PRINT : PRINT 
+880  PRINT : PRINT
 890 XD = (XB - XA) / (HH - 1):YD = (YB - YA) / (BR - 1):PT = 0
 900  FOR NX = 0 TO NL: FOR NY = 0 TO NL
-910 PX%(NX,NY) = (X(PT) - XA) / XD + .5:PY%(NX,NY) = (Y(PT) - YA) / YD + .5: POKE 1024,NX: POKE 1025,NY:PT = PT + 1: NEXT : NEXT 
-920  HGR2 
+910 PX%(NX,NY) = (X(PT) - XA) / XD + .5:PY%(NX,NY) = (Y(PT) - YA) / YD + .5: POKE 1024,NX: POKE 1025,NY:PT = PT + 1: NEXT : NEXT
+920  HGR2
 930  FOR NY = NL - 1 TO 0 STEP  - 1:XA = PX%(NL,NY + 1):YA = PY%(NL,NY + 1)
-940 XB = PX%(NL,NY):YB = PY%(NL,NY): GOSUB 20: NEXT 
+940 XB = PX%(NL,NY):YB = PY%(NL,NY): GOSUB 20: NEXT
 950  FOR NX = NL - 1 TO 0 STEP  - 1:XA = PX%(NX + 1,NL):YA = PY%(NX + 1,NL)
 960 XB = PX%(NX,NL):YB = PY%(NX,NL): GOSUB 20: FOR NY = NL - 1 TO 0 STEP  - 1
 970 XA = PX%(NX + 1,NY):YA = PY%(NX + 1,NY):XB = PX%(NX,NY):YB = PY%(NX,NY)
 980 XD = XB - XA:YD = YB - YA:S =  SQR ((XD * XD + YD * YD) / 2): IF S = 0 THEN 1000
 990 XB =  INT (XB - XD / S + .5):YB =  INT (YB - YD / S + .5): GOSUB 20
 1000 XA = PX%(NX,NY + 1):YA = PY%(NX,NY + 1):XB = PX%(NX,NY):YB = PY%(NX,NY): GOSUB 20
-1010  NEXT : NEXT 
+1010  NEXT : NEXT
 1020  TEXT : PRINT "J'AI FINI LES CALCULS."
-1030  GET A$: IF A$ = " " THEN  END 
+1030  GET A$: IF A$ = " " THEN  END
 1040  GOTO 1030
 */
 import stdio;
@@ -93,8 +75,8 @@ float[] X, Y;                                       // 500  DIM X((NL + 1) ^ 2 -
 
 void GOTO470()
 {
-    foreach(i; O) O[i] = -1;                        // 470  FOR I = 0 TO BR - 1:O%(I) =  - 1: NEXT 
-    writeln("\n");                                  // 480  PRINT : PRINT 
+    foreach(i; O) O[i] = -1;                        // 470  FOR I = 0 TO BR - 1:O%(I) =  - 1: NEXT
+    writeln("\n");                                  // 480  PRINT : PRINT
     INPUT("NOMBRE DE MERIDIENS:", NL);              // 490  INPUT "NOMBRE DE MERIDIENS:";NL
     PX = new int[][](NL, NL);                       // 500  DIM PX%(NL,NL),PY%(NL,NL),X((NL + 1) ^ 2 - 1),Y((NL + 1) ^ 2 - 1)
     PY = new int[][](NL, NL);
@@ -103,24 +85,77 @@ void GOTO470()
     do {
         INPUT("\nDEBUT D'INTERVALLE SUR X:", X0);   // 510  PRINT : INPUT "DEBUT D'INTERVALLE SUR X:";X0
         INPUT("FIN D'INTERVALLE SUR X:", X1);       // 520  INPUT "FIN D'INTERVALLE SUR X:";X1
-        writeln;                                    // 530  PRINT 
+        writeln;                                    // 530  PRINT
     } while(X1 <= X0);                              // 540  IF X1 <  = X0 THEN 510
     do {
         INPUT("DEBUT D'INTERVALLE SUR Y:", Y0);     // 550  INPUT "DEBUT D'INTERVALLE SUR Y:";Y0
         INPUT("FIN D'INTERVALLE SUR Y:", Y1);       // 560  INPUT "FIN D'INTERVALLE SUR Y:";Y1
-        writeln;                                    // 570  PRINT 
+        writeln;                                    // 570  PRINT
     } while(Y1 <= Y0);                              // 580  IF Y1 <  = Y0 THEN 550
     do {
         writeln("ANGLE DE VISION DU COTE");         // 590  PRINT "ANGLE DE VISION DU COTE"
         INPUT("(AZIMUTE 0-80)", AZ);                // 600  INPUT "(AZIMUTE 0-80)";AZ
-        writeln;                                    // 610  PRINT 
+        writeln;                                    // 610  PRINT
     } while(AZ < 0 && AZ > 80);                     // 620  IF AZ < 0 OR AZ > 80 THEN 590
     do {
         INPUT("INCLINAISON, HAUTEUR (0-90)", PH);   // 630  INPUT "INCLINAISON, HAUTEUR (0-90)";PH
-        writeln;                                    // 640  PRINT 
+        writeln;                                    // 640  PRINT
     } while(PH < 0 && PH > 90);                     // 650  IF PH < 0 OR PH > 90 THEN 630
-        writeln;                                    // 660  PRINT 
+        writeln;                                    // 660  PRINT
+}
 
+void GOTO670()
+{
+  P1 = cos(AZ * PI / 180.0);
+  P2 = sin(AZ * PI / 180.0);                     // 670 P1 =  COS (AZ * PI / 180):P2 =  SIN (AZ * PI / 180)
+  P3 = cos(PH * PI / 180.0);                     // 680 P3 =  COS (PH * PI / 180)
+  P4 = P1 * sin(PH * PI / 180.0);                // 690 P4 = P1 *  SIN (PH * PI / 180)
+  P5 = P2 * sin(PH * PI / 180.0);                // 700 P5 = P2 *  SIN (PH * PI / 180)
+  writeln("JE CHERCHE MAXIMUM ET MINIMUM.\n\n"); // 710  PRINT "JE CHERCHE MAXIMUM ET MINIMUM."
+                                                 // 720  PRINT
+  XS = (X1 - X0) / NL;                           // 730 XS = (X1 - X0) / NL:YS = (Y1 - Y0) / NL
+  YS = (Y1 - Y0) / NL;
+  X = X0;
+  Y = Y0;
+  GOSUB200;
+  Z0 = Z;                                        // 740 X = X0:Y = Y0: GOSUB 200:Z0 = Z:Z1 = Z:XA = XX:XB = XX:YA = YY:YB = YY:PT = 0
+  Z1 = Z;
+  XA = XX;
+  XB = XX;
+  YA = YY;
+  YB = YY;
+  PT = 0;                                         // 750 X = X0 - XS: FOR NX = 0 TO NL:X = X + XS:Y = Y0 - YS: FOR NY = 0 TO NL:Y = Y + YS: GOSUB 200:X(PT) = XX:Y(PT) = YY: POKE 1024,NX: POKE 1025,NY
+  X = X0 - XS;
+  for(NX=0; NX<=NL; NX++) {
+    X = X + XS;
+    Y = Y0 - YS;
+    for(NY=0; NY<=NL; NY++) {
+      Y = Y + YS;
+      GOSUB200;
+      X(PT) = XX;
+      Y(PT) = YY;
+      POKE(1024, NX);
+      POKE(1025, NY);
+      if(Z > Z1)                                  // 760  IF Z > Z1 THEN Z1 = Z: GOTO 780
+        Z1 = Z;
+      else
+        if(Z < Z0)                                // 770  IF Z < Z0 THEN Z0 = Z
+          Z0 = Z;
+      if(XA > XX)                                 // 780  IF XA > XX THEN XA = XX: GOTO 800
+        XA = XX;
+      else
+        if(XX > XB)                               // 790  IF XX > XB THEN XB = XX
+          XB = XX;
+      if(YA > YY)                                 // 800  IF YA > YY THEN YA = YY: GOTO 820
+        YA = YY;
+      else
+        if(YY > YB)                               // 810  IF YY > YB THEN YB = YY
+          YB = YY;
+      PT = PT + 1;                                // 820 PT = PT + 1: NEXT : NEXT
+    }
+  }
+  writeln("MINIMUM Z ", Z0);                     // 830  PRINT "MINIMUM Z ";Z0
+  writeln("MAXIMUM Z ", Z1);                     // 840  PRINT "MAXIMUM Z ";Z1
 }
 
 /* Read a response from the keyboard */
@@ -157,7 +192,9 @@ int main(string[] args)
     writeln("EN SERA ETABLIE A L'ECRAN.\n\n");          // 420  PRINT "EN SERA ETABLIE A L'ECRAN."
     writeln("LA FONCTION EST-ELLE DEJA DEFINIE ";       // 440  PRINT : PRINT "LA FONCTION EST-ELLE DEJA DEFINIE ";: GOSUB 2000
     GOSUB2000();
-    if(A == 'O')                                        // 150  IF A$ = "O" THEN 470
-      THEN GOTO470;
-    return 0;                                           // 460  END 
+    if(A == 'O') {                                       // 150  IF A$ = "O" THEN 470
+      GOTO470;
+      GOTO670;
+    }
+    return 0;                                           // 460  END
 }
